d130cb8f61b4bfd362318ce69e4c53882e011e45
pcy <aintelligence1022@gmail.com>
2025-05-17 15:32:15 +0900

    extension

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+try:
+    from plyer import notification
+except:
+    import os
+    os.system("pip install plyer")
+    from plyer import notification
+
+notification.notify(
+    title = "tmp title",
+    message = "sample message",
+    )
